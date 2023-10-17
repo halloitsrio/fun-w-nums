a362590a2436e40d94511da2714272ac81515872
halloitsrio <kanwar7970@mydusd.org>
2023-10-17 11:10:38 -0700

    added oddEven method

--- FunNums.java
+++ FunNums.java
@@ -1,5 +1,6 @@
 /*
  * add comments on authors, data, version, & purpose 
+ * need to add import for scanner & anything else that is needed
 */
 
 public class FunNums {
@@ -24,10 +25,24 @@ public class FunNums {
     }
   }
 
+  /*
+   * oddEven is a static method that prints out a statemnt that tells the user whether the number they inputted is odd or even
+   * @param num - the input number that is tested to be odd or even
+   */
+  public static void oddEven(int num) {
+    if (num % 2 ==0) {
+      System.out.println(num + " is an even number.");
+    } else {
+      System.out.println(num + " is an odd number.");
+    }
+  }
+
   //Main
   public static void main(String[] args) {
     //System.out.println("test");
     isPrime(13);
     isPrime(12);
+    oddEven(13);
+    oddEven(12);
   }
 }
