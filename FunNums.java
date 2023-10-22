1c4234e552b2835a3dd4709d8ce7e8ae7e335d71
halloitsrio <kanwar7970@mydusd.org>
2023-10-22 16:43:15 -0700

    shreya code

--- FunNums.java
+++ FunNums.java
@@ -38,13 +38,44 @@ public class FunNums {
   }
 
   /*
-   * lcm is a static method that prints out a statement that tells the user the least common multiple of the two numbers that they inputted
-   * @param num1 - the first number that the user inputs that is used to find the lcm
-   * @param num2 - the second number that the user inputs that is used to find the lcm
-   */
-  public static void lcm(int num1, int num2){
-    
+     * lcm is a static method that prints out a statement that tells the user the least common multiple of the two numbers that they inputted
+     * @param num1 - the first number that the user inputs that is used to find the LCM
+     * @param num2 - the second number that the user inputs that is used to find the LCM
+     */
+    public static void lcm(int num1, int num2) {
+      int smallerNumber;
+      if (num1 < num2) {
+          smallerNumber = num1;
+      } else {
+          smallerNumber = num2;
+      }
+
+      int count = smallerNumber; // Initialize count with the smaller number
+
+      while (true) {
+          if (count % num1 == 0 && count % num2 == 0) {
+              System.out.println("The LCM of " + num1 + " and " + num2 + " is " + count);
+              break; // Found the LCM, exit the loop
+          }
+          count++;
+      }
   }
+
+/*
+   * gcf is a static method that calculates and returns the greatest common factor of two numbers.
+   * @param num1 - the first number
+   * @param num2 - the second number
+   */
+  public static void gcf(int num1, int num2) {
+    System.out.print("The GCF of " + num1 + " and " + num2 + " is ");
+      while (num2 != 0) {
+          int temp = num2;
+          num2 = num1 % num2;
+          num1 = temp;
+      }
+      System.out.println(num1);
+  }
+
 
   //Main
   public static void main(String[] args) {
@@ -53,5 +84,12 @@ public class FunNums {
     isPrime(12);
     oddEven(13);
     oddEven(12);
+    int num1 = 12;
+    int num2 = 18;
+    lcm(num1, num2);
+    num1 = 24;
+    num2 = 18;
+    gcf(num1, num2);
+
   }
 }
